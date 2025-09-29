9d5b7282fa3e9b74a81ba07a388e59de694407af
LeafyCloud01 <162726080+LeafyCloud01@users.noreply.github.com>
2025-09-29 18:00:39 +0200

    Added some basic elements to the UI

--- DocumentManagementSystem/DocumentManagementUI/src/App.jsx
+++ DocumentManagementSystem/DocumentManagementUI/src/App.jsx
@@ -1,12 +1,34 @@
 import { useState } from 'react'
 import './App.css'
 
+// install react router for navigation
+// npm install react-router-dom
+
+function Documents() {
+    return (
+        <>
+
+        </>
+    );
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
   return (
     <>
-      
+          <h1>Document Management System</h1>
+
+          <input id="search" type="text" /> {/* Change to use form & react input syntax */}
+          <br /><br />
+
+          <input id="document" type="file" /> {/* Change to use form & react input syntax */}
+          <button onClick={ }></button>
+          <br /><br />
+
+          <div id="document_list"> {/* have it return table of all documents (that match search) along with update,delete & metadata buttons for each */}
+          <Documents />
+          </div>
     </>
   )
 }
